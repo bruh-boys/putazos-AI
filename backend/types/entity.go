bddff37f36d5a143cb25cee00dd50871d370ba25
FlamesX-128 <glowingorangeflames@gmail.com>
2022-10-08 23:19:20 -0500

    planning the structures, soldier, entity.

--- backend/types/entity.go
+++ backend/types/entity.go
@@ -19,28 +19,36 @@ func (s Soldier) HasAction(ss string) bool {
 
 type World struct{}
 
-func (w World) GenerateEntity() {
+func (w World) GenerateEntity(s string) bool {
 
+	return false
 }
 
 type Entity[T Soldier] struct {
 	ID string
 
-	Sprites map[string]string
-	States  map[string]func()
-	State   string
+	Sprites   map[string]string
+	States    map[string]func()
+	PrevState string
+	State     string
 
-	Entity T
+	World  *World
+	Entity *T
 }
 
 func (e *Entity[any]) ChangeState(state string) bool {
 	if _, ok := e.States[state]; ok == true {
+		e.PrevState = e.State
 		e.State = state
 
 		return true
 	}
 
 	return false
+}
+
+func (e *Entity[any]) SetPreviosState() {
+	e.State = e.PrevState
 }
 
 func Contains[T string](s T, ss []T) bool {
@@ -71,6 +79,7 @@ var SoldierStates = map[string]func(e Entity[Soldier]){
 
 	},
 	"attack": func(e Entity[Soldier]) {
-
+		e.World.GenerateEntity("bullet")
+		e.SetPreviosState()
 	},
 }
