85db91111ab90c8393cfeeed74fbc7c776a926fa
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-10-09 14:41:59 -0500

    Update world.go

--- backend/game/world.go
+++ backend/game/world.go
@@ -6,13 +6,16 @@ type Platform struct {
 	Base   float64
 	Height float64
 }
+
 type World struct {
 	Platforms []Platform
 	Soldiers  []Soldier
 }
 
 func (w World) SoldierIsOnPlatform(soldier Soldier) float64 {
+	lowy := 10000.0
 	y := 0.0
+
 	for i := 0; i < len(w.Platforms); i++ {
 
 		// is up or down?
@@ -20,7 +23,10 @@ func (w World) SoldierIsOnPlatform(soldier Soldier) float64 {
 			(soldier.X <= (w.Platforms[i].X+w.Platforms[i].Base) && soldier.X >= w.Platforms[i].X) {
 			y = w.Platforms[i].Y + w.Platforms[i].Height
 		}
+		if lowy < y {
+			lowy = y
+		}
 
 	}
-	return y
+	return lowy
 }
