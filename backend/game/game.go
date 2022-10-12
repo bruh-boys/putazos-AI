b1f087d0ce21d234450207f4e0eac08c4aff4953
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-10-11 19:35:38 -0500

    yep changes

--- backend/game/game.go
+++ backend/game/game.go
@@ -1,5 +1,23 @@
 package game
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Game struct {
-	W World
+	World       World      `json:"world"`
+	Soldiers    []Soldier  `json:"soldiers"`
+	SpawnPoints []Platform `json:"spawn-points"`
+	GameMap     []float64  `json:"game-map"`
+}
+
+func NewGame(name string) (g Game) {
+	file, _ := os.Open(name)
+	json.NewDecoder(file).Decode(&g)
+	return
+}
+func DoSomething() {
+	fmt.Println("idk ")
 }
