a765680f2f670529adce2ca599828518f40d6e15
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-11-08 14:39:01 -0600

    rnn

--- backend/brain/network.go
+++ backend/brain/network.go
@@ -1,86 +1,194 @@
 package brain
 
 import (
+	"encoding/json"
 	"math/rand"
+	"os"
+	"time"
 )
 
-type Network struct {
-	Bias            [][]float64
-	Weights         [][][]float64
-	ActivationFuncs []string
+type NN struct {
+	Weights         [][][]float32 `json:"weights"          `
+	Bias            [][]float32   `json:"bias"             `
+	ActivationFuncs []string      `json:"activation-funcs" `
+	Comment         string        `json:"comment" `
 }
 
-func NewNetwork(layers []int, activationFuncs []string) (net Network) {
-	net.ActivationFuncs = activationFuncs
-	net.Bias = make([][]float64, len(layers)-1)
-	net.Weights = make([][][]float64, len(layers)-1)
-	// this only works for defining the bias and the weights
-	for l := 0; l < len(layers)-1; l++ {
-		// its really simple and you should know why this look like this
-		net.Weights[l] = make([][]float64, layers[l])
-		net.Bias[l] = make([]float64, layers[l+1])
-		// each neuron have a connection that have a weight
-		for n := 0; n < layers[l]; n++ {
-			net.Weights[l][n] = make([]float64, layers[l+1])
-			for c := 0; c < layers[l+1]; c++ {
-				net.Weights[l][n][c] = rand.Float64() - 0.5
+func NewNeuralNetwork(neuronsPerLayer []int, activationFuncs []string, comment string) NN {
+	rand.Seed(time.Now().Unix())
+	if len(activationFuncs)+1 < len(neuronsPerLayer) {
+		panic("the activation funcs are different")
+
+	}
+	// the output doesnt have a weight
+	// the same for the bias
+	weights := make([][][]float32, len(neuronsPerLayer)-1)
+	bias := make([][]float32, len(neuronsPerLayer)-1)
+
+	for i := 0; i < len(neuronsPerLayer)-1; i++ {
+		// this add new weights for each neuron
+		for n := 0; n < neuronsPerLayer[i]; n++ {
+			weights[i] = append(weights[i], []float32{})
+			// as you should know the weights of the neuron connects to the next layer
+			// so this is really basic
+			for w := 0; w < neuronsPerLayer[i+1]; w++ {
+				weights[i][n] = append(weights[i][n], rand.Float32()-0.5)
 			}
+
 		}
-		// the input doesnt have a bias , this is only for the hidden layers and the last layer
-		for n := 0; n < layers[l+1]; n++ {
-			net.Bias[l][n] = rand.Float64() - 0.5
+		// because the input doesnt have a bias , this is why it looks like this
+		for n := 0; n < neuronsPerLayer[i+1]; n++ {
+			bias[i] = append(bias[i], rand.Float32()-0.5)
+		}
+	}
+
+	return NN{Weights: weights, Bias: bias, ActivationFuncs: activationFuncs, Comment: comment}
+}
+
+// this is like the predict function
+// but it returns you the layers
+
+func (net NN) FeedFoward(input []float32, feedBack [][]float32) (layers [][]float32, feed [][]float32) {
+	layers = make([][]float32, len(net.Bias)+1)
+	feed = make([][]float32, len(net.Bias)-1)
+	layers[0] = make([]float32, len(input))
+	copy(layers[0], input)
+
+	for l := 0; l < len(layers)-1; l++ {
+		layers[l+1] = make([]float32, len(net.Bias[l]))
+
+		// layer*weight
+		for n := 0; n < len(layers[l]); n++ {
+
+			for i, w := range net.Weights[l][n] {
+				var q float32
+				if l != len(net.Bias)-1 && feedBack != nil && l != 0 {
+					q = (feedBack[l-1][n])
+
+				}
+				layers[l+1][i] += w * (layers[l][n] + q)
+
+			}
+
+		}
+		//layer(l+1)=f(bias)
+		if l != len(net.Bias)-1 && l != 0 {
+
+			feed[l-1] = make([]float32, len(layers[l]))
+			copy(feed[l-1], layers[l])
+
+		}
+		for i := range layers[l+1] {
+
+			layers[l+1][i] = mathFuncs[net.ActivationFuncs[l]]["activate"](layers[l+1][i] + net.Bias[l][i])
+
 		}
 
 	}
 	return
 }
 
-func (net Network) Foward(input []float64) []float64 {
-	output := []float64{}
+// reutiliza la memoria, para poder generar una cadena de acciones
+func (net NN) Predict(input []float32, memory [][]float32) ([]float32, [][]float32) {
+	lays, feed := net.FeedFoward(input, memory)
+	return lays[len(lays)-1], feed
+
+}
+
+func (net *NN) BackPropagation(layers [][]float32, memory [][]float32, expected []float32) ([][][]float32, [][]float32) {
+
+	bd := make([][]float32, len(net.Bias))
+	wd := make([][][]float32, len(net.Weights))
+	errors := make([]float32, len(expected))
+	layer := layers[len(layers)-1]
+	// I dont need to explain this one
+	for i, n := range layer {
+		errors[i] = (n - expected[i])
+	}
+
+	for l := len(net.Bias) - 1; l >= 0; l-- {
+		bd[l] = make([]float32, len(net.Bias[l]))
+		wd[l] = make([][]float32, len(net.Weights[l]))
+
+		for i := range net.Bias[l] {
+			//gradient=errors*dy/dx(fx)(layer[l+1])
+
+			bd[l][i] += ((errors[i]) * mathFuncs[net.ActivationFuncs[l]]["derivative"](layer[i]))
+		}
+		//layer_t *gradient
+		for n := 0; n < len(wd[l]); n++ {
+			wd[l][n] = make([]float32, len(net.Weights[l][n]))
+			var q float32
+			if l != len(net.Bias)-1 && memory != nil && l != 0 {
+				q = (memory[l-1][n])
+
+			}
+			for i := range net.Weights[l][n] {
+
+				wd[l][n][i] += (layers[l][n] + q) * (bd[l][i])
+			}
+		}
+
+		if l == 0 {
+			continue
+		}
+
+		layer = layers[l]
+		errorcp := make([]float32, len(layer))
+		// errors=weights_t*errors
+		for i := range layer {
+
+			var err float32 = 0.0
+			for j := range errors {
+				err += net.Weights[l][i][j] * errors[j]
+			}
+			errorcp[i] = err
+		}
+		errors = errorcp
+
+	}
+
+	return wd, bd
+}
+
+// so , this update the weights and bias
+// yeah its really simple
+// the size is for the importance of each gradient
+func (net *NN) UpdateWeightAndBias(size, learningRate float32, weightsGrad [][][]float32, biasGrad [][]float32) {
+
 	for l := 0; l < len(net.Weights); l++ {
-		output = make([]float64, len(net.Bias[l]))
-		copy(output, net.Bias[l])
-
-		// this is really basic stuff, is jsut
-		// input*weights(matrix multiplication)
 		for n := 0; n < len(net.Weights[l]); n++ {
-			for c := 0; c < len(net.Weights[l][n]); c++ {
-				output[c] += input[n] * net.Weights[l][n][c]
+			for i := range net.Weights[l][n] {
+				// this reduce the error
+				net.Weights[l][n][i] -= ((weightsGrad[l][n][i]) * learningRate) / size
 			}
 		}
-		// activation(output)
-		for n := 0; n < len(output); n++ {
-			output[n] = mathFuncs[net.ActivationFuncs[l]](output[n])
+		// same for this
+		for i := range net.Bias[l] {
+			net.Bias[l][i] -= (biasGrad[l][i] * learningRate) / size
 		}
-		input = make([]float64, len(output))
-		copy(input, output)
+
 	}
-	return output
+
 }
 
-func (net *Network) Mutate(netCopy Network) {
-	for l := 0; l < len(net.Bias); l++ {
-		for n := 0; n < len(net.Weights[l]); n++ {
-			for c := 0; c < len(net.Weights[l][n]); c++ {
+// the model is saved in a json format
+func (net *NN) SaveModel(name string) {
 
-				net.Weights[l][n][c] = randomValue(netCopy.Weights[l][n][c])
+	f, _ := os.Create(name)
 
-			}
-		}
-		for n := 0; n < len(net.Bias[l]); n++ {
+	json.NewEncoder(f).Encode(net)
 
-			net.Bias[l][n] = randomValue(netCopy.Bias[l][n])
-
-		}
-	}
 }
 
-func randomValue(val float64) float64 {
-	// it only have a 10% of probability of having another value
-	if rand.Float64() < 0.1 {
-		return rand.Float64() - 0.5
-
+// json format
+func OpenModel(name string) NN {
+	var net NN
+	f, err := os.Open(name)
+	if err != nil {
+		panic(name + " doesnt exist ")
 	}
-	// this doesnt change too much
-	return val + rand.NormFloat64()
+	json.NewDecoder(f).Decode(&net)
+	return net
+
 }
